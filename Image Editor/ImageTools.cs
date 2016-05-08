e8640463d01260ddff55dfe28e1a1aae0a53e368
Orest Kosyk <orest.kosyk@outlook.com>
2016-05-09 00:20:45 +0300

    realiztion of brush tool logic

--- Image Editor/ImageTools.cs
+++ Image Editor/ImageTools.cs
@@ -56,7 +56,6 @@ namespace Image_Editor
             Bitmap bmp = new Bitmap(image.Width, image.Height, PixelFormat.Format24bppRgb);
             Graphics g = Graphics.FromImage(bmp);
             g.DrawImageUnscaled(image.ActiveLayerBitmap, 0, 0);
-            g.Dispose();
             image.ActiveLayerBitmap = bmp;
         }
         public static void ToARGB(EditableImage image)
